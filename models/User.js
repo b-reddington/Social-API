d93663ef83a113ac9fcae2c86552e24eac4d2ecf
b-reddington <brandon.reddington98@gmail.com>
2023-05-08 00:51:37 -0700

    Added User Model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^\S+@\S+\.\S+$/,
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+        toJSON: {
+            virtuals: true,
+        },
+    }
+)
+
+const User = model('user', userSchema);
+module.exports = User;
